7e522181b2f76fa5479b70a71057fcb549d9e7fb
triptocode <3580rio@gmail.com>
2022-11-24 00:20:47 +0900

    롬복의 @Data @AllArgsConstructor : get, set, 생성자 등을 자동생성해줌

--- src/main/java/com/example/adminStudy/model/SearchParam.java
+++ src/main/java/com/example/adminStudy/model/SearchParam.java
@@ -1,5 +1,10 @@
 package com.example.adminStudy.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+// 16번줄이하 주석처리한 get, set 메소드 외에 생성자까지 자동생성해주는 lombok 의 @Data 와 @AllArgConstructor
+@Data
+@AllArgsConstructor
 public class SearchParam {
 
     private String account;
@@ -7,28 +12,14 @@ public class SearchParam {
     private int page;
 
 
-    public String getAccount() { return account; }
 
-    public void setAccount(String account) {
-        this.account = account;
-    }
+//    public String getAccount() { return account; }
+//    public void setAccount(String account) { this.account = account;}
 
+//    public String getEmail() { return email; }
+//    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
-    }
+//    public int getPage() { return page; }
+//    public void setPage(int page) { this.page = page; }
 
 }
