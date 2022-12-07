0fd06c63184adb65a672334c71ffe25fb1a7554a
triptocode <3580rio@gmail.com>
2022-12-07 14:23:08 +0900

    user.ifPresent(selectUser-> {userRepository.delete(selectUser);} , if(deleteUser.isPresent())

--- src/test/java/com/example/adminStudy/repository/UserRepositoryTest.java
+++ src/test/java/com/example/adminStudy/repository/UserRepositoryTest.java
@@ -24,11 +24,11 @@ public class UserRepositoryTest extends AdminStudyApplicationTests {
     public void create(){
         //String sql = insert into user(%s, %s, %d) value (account, email, age);
        User user = new User();
-       user.setAccount("TestUser02");
-       user.setEmail("TestUser02@gmail.com");
-       user.setPhoneNumber("010-1111-2222");
+       user.setAccount("TestUser03");
+       user.setEmail("TestUser03@gmail.com");
+       user.setPhoneNumber("010-3333-3333");
        user.setCreatedAt(LocalDateTime.now());
-       user.setCreatedBy("TestUser2");
+       user.setCreatedBy("TestUser3");
 
        User newUser = userRepository.save(user);
         System.out.println("newUser: " + newUser);
@@ -52,7 +52,18 @@ public class UserRepositoryTest extends AdminStudyApplicationTests {
             userRepository.save(selectUser);
         });
     }
+    @Test
     public void delete(){
+        Optional<User> user = userRepository.findById(2L);  // 조회
+        user.ifPresent(selectUser-> {userRepository.delete(selectUser);}
+                        );
+        Optional<User> deleteUser = userRepository.findById(2L);
+        if(deleteUser.isPresent()){
+            System.out.println("데이터 존재 : " + deleteUser.get());
+        }else{
+            System.out.println( "데이터 삭제로 데이터 없음"
+            );
+        }
 
     }
 }
